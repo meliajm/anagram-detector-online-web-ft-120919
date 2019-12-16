72c89c7b98893ba0552326af2b0add2950afa22d
melia miller <meliajm@gmail.com>
2019-12-16 02:39:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word) 
+    @word = word
+  end
+  
+  def match(array)
+    # make word into array, then sort it 
+    # iterate through array, make each word an array, then sort
+    # then compare word array to array array 
+    # add results to new array 
+    result_array = [] # this is array of matches
+    w = @word.split(", ")
+    
+    
+    
+    result_array
+    
+  end 
+  
+end
